5ce54ae6c4b9b134c192dfff61a3e83c580b3e7a
Micheal Kabwe <kabwemusakanya@gmail.com>
2020-12-16 23:03:47 +0100

    added basic styling and component setup

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+// import Rect from 'react';
+import Directory from '../Components/Directory'
+const HomePage = ()=>{
+    return(
+        <div className='Home-Page'>
+            <Directory />
+        </div>
+    )
+}
+
+
+export default HomePage;
